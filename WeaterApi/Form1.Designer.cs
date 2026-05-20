01530f15d87a9801dd1e35a9e2bfb5a3ea848e46
ramimislam676 <mdramimislam6332@gmail.com>
2026-05-21 00:54:34 +0600

    completed weather info class and added an image in the frame

--- WeaterApi/Form1.Designer.cs
+++ WeaterApi/Form1.Designer.cs
@@ -28,46 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(153, 455);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(164, 68);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(388, 455);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(164, 68);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(612, 455);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(164, 68);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(961, 603);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(932, 606);
+            this.DoubleBuffered = true;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.Name = "Form1";
@@ -78,10 +49,6 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
     }
 }
 
--- WeaterApi/WeatherInfo.cs
+++ WeaterApi/WeatherInfo.cs
@@ -8,5 +8,52 @@ namespace WeaterApi
 {
     internal class WeatherInfo
     {
+        public class coord
+        {
+            public double lon { get; set; }
+            public double lat { get; set; }
+        }
+
+        public class weather
+        {
+           // public int id { get; set; }
+            public string main { get; set; }
+            public string description { get; set; }
+            public string icon { get; set; }
+        }
+
+        public class main
+        {
+            public double temp { get; set; }
+           // public double feels_like { get; set; }
+           // public double temp_min { get; set; }
+           // public double temp_max { get; set; }
+            public int pressure { get; set; }
+            public int humidity { get; set; }
+        }
+
+        public class wind
+        {
+            public double speed { get; set; }
+            //public int deg { get; set; }
+        }
+
+        public class sys
+        {
+            //public int type { get; set; }
+           // public int id { get; set; }
+           // public string country { get; set; }
+            public int sunrise { get; set; }
+            public int sunset { get; set; }
+        }
+
+        public class root
+        {
+            public coord coord { get; set; }
+            public List<weather> weather { get; set; }
+            public main main { get; set; }
+            public wind wind { get; set; }
+            public sys sys { get; set; }
+        }
     }
 }
